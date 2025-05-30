f886e563c28b1272f66f387855ada0b80b308a3a
angel24r <luisangel.ry24@gmail.com>
2025-05-29 20:29:05 -0600

    Error en archivos

--- database/migrations/2025_05_29_173231_create_tasks_table.php
+++ database/migrations/2025_05_29_173231_create_tasks_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
             $table->text('description')->nullable();
             $table->boolean('completed')->default(false);
             $table->foreignId('task_list_id')->constrained()->onDelete('cascade');
